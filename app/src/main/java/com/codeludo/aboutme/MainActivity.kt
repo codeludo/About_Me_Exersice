7b1cebe3e0cecb9b4039cb9148fc159663cb4e56
Camilo <jcam.qvision@gmail.com>
2022-05-16 17:29:59 -0500

    use data binding to change nickname

--- app/src/main/java/com/codeludo/aboutme/MainActivity.kt
+++ app/src/main/java/com/codeludo/aboutme/MainActivity.kt
@@ -2,31 +2,41 @@ package com.codeludo.aboutme
 
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
+import com.codeludo.aboutme.databinding.ActivityMainBinding
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.ViewAnimator
 import androidx.databinding.DataBindingUtil
-import com.codeludo.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // binding variable
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var done_button: Button
     private lateinit var nickname_text: TextView
     private lateinit var nickname_edit: EditText
 
+    // create data
+    private val myName: MyName = MyName("Jason Statham")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // binding object
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+
+        // using binding to reference views
         nickname_text = binding.nicknameText
         nickname_edit = binding.nicknameEdit
         done_button = binding.doneButton
+
+        // binding created data
+        binding.myName = myName
 
         done_button.setOnClickListener {
             addNickname(it)
@@ -41,7 +51,12 @@ class MainActivity : AppCompatActivity() {
     private fun addNickname(view: View){
 
         // agregar nickname
-        nickname_text.text = nickname_edit.text
+
+        binding.apply {
+            myName?.nickname = nickname_edit.text.toString()
+            invalidateAll()
+        }
+
         // esconder editText
         nickname_edit.visibility = View.GONE
         // esconder boton
